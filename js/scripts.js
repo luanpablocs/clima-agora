e0d255b085273deb6137c05b8243594336826be4
Luan Pablo <luanpablo@eadoo.tec.br>
2022-09-22 10:10:35 -0400

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
+const weatherContainer = document.querySelector("#weather-data")
+
 // Funções
 const getWeatherData = async (city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
@@ -36,13 +38,23 @@ const showWeatherData = async (city) => {
     countryElement.setAttribute("src", apiCountryURL + data.sys.country);
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide")
 };
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-        const city = cityInput.value;
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter"){
+        const city = e.target.value;
 
         showWeatherData(city);
+    }
 });
